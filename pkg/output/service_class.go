7a246025c3766ee37ee90c0074efa5440f09485e
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-11-21 11:34:49 -0600

    Add catalog class subcommand (#21)
    * Add detail and summary table formatters
    
    These are specialized tablewriters that are optimized for displaying
    either a list of results, or a single item. A lot of the "noise" from
    the borders have been removed.
    
    * Move catalog list to catalog class list
    
    We are no longer going to put verbs directly under catalog. Instead we
    are using the "catalog" command to group together catalog level concepts,
    class and plan, vs cluster level concepts, like instance and binding.
    
    * Implement catalog class get [--traverse]

--- pkg/output/service_class.go
+++ pkg/output/service_class.go
@@ -1,9 +1,18 @@
 package output
 
 import (
+	"strings"
+
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
 	"github.com/olekukonko/tablewriter"
 )
+
+func getStatusText(status v1beta1.ClusterServiceClassStatus) string {
+	if status.RemovedFromBrokerCatalog {
+		return "deprecated"
+	}
+	return "active"
+}
 
 // ClusterServiceClassHeaders sets the appropriate headers on t for displaying
 // ClusterServiceClasses in t
@@ -23,4 +32,35 @@ func AppendClusterServiceClass(table *tablewriter.Table, class *v1beta1.ClusterS
 		class.Spec.Description,
 		class.Name,
 	})
+}
+
+// WriteClusterServiceClassList prints a list of service class to the console.
+func WriteClusterServiceClassList(classes []v1beta1.ClusterServiceClass) {
+	t := NewListTable()
+	t.SetHeader([]string{
+		"Name",
+		"Description",
+		"UUID",
+	})
+	for _, class := range classes {
+		t.Append([]string{
+			class.Spec.ExternalName,
+			class.Spec.Description,
+			class.Name,
+		})
+	}
+	t.Render()
+}
+
+// WriteClusterServiceClassDetails prints a service class to the console.
+func WriteClusterServiceClass(class *v1beta1.ClusterServiceClass) {
+	t := NewDetailsTable()
+	t.AppendBulk([][]string{
+		{"Name:", class.Spec.ExternalName},
+		{"Description:", class.Spec.Description},
+		{"UUID:", string(class.Name)},
+		{"Status:", getStatusText(class.Status)},
+		{"Tags:", strings.Join(class.Spec.Tags, ", ")},
+	})
+	t.Render()
 }
