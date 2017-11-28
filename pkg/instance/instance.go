d48f33c5a6140b797f79480a4d126c73ab314547
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2017-11-28 15:09:51 -0600

    Standardize instance output

--- pkg/instance/instance.go
+++ pkg/instance/instance.go
@@ -8,6 +8,15 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func retrieveAll(cl *clientset.Clientset, ns string) (*v1beta1.ServiceInstanceList, error) {
+	instances, err := cl.ServicecatalogV1beta1().ServiceInstances(ns).List(v1.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("Error listing instances (%s)", err)
+	}
+
+	return instances, nil
+}
+
 func retrieveByName(cl *clientset.Clientset, ns, name string) (*v1beta1.ServiceInstance, error) {
 	instance, err := cl.ServicecatalogV1beta1().ServiceInstances(ns).Get(name, v1.GetOptions{})
 	if err != nil {
--- pkg/output/instance.go
+++ pkg/output/instance.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
-	"github.com/olekukonko/tablewriter"
 )
 
 func getInstanceStatusCondition(status v1beta1.ServiceInstanceStatus) v1beta1.ServiceInstanceCondition {
@@ -24,34 +23,28 @@ func getInstanceStatusShort(status v1beta1.ServiceInstanceStatus) string {
 	return string(lastCond.Type)
 }
 
-// InstanceHeaders sets the appropriate headers on t for displaying ServiceInstances
-// in t
-func InstanceHeaders(t *tablewriter.Table) {
+// WriteInstanceList prints a list of instances.
+func WriteInstanceList(instances ...v1beta1.ServiceInstance) {
+	t := NewListTable()
 	t.SetHeader([]string{
 		"Name",
-		"Service Class Name",
-		"Service Class UUID",
-		"Service Plan Name",
-		"Service Plan UUID",
+		"Namespace",
+		"Class",
+		"Plan",
 		"Status",
 	})
-}
 
-// AppendInstance appends instance to t by calling t.Append.
-// Ensure that you've called InstanceHeaders on t before you call AppendInstance
-func AppendInstance(t *tablewriter.Table, instance *v1beta1.ServiceInstance) {
-	latestCond := "None"
-	if len(instance.Status.Conditions) >= 1 {
-		latestCond = instance.Status.Conditions[len(instance.Status.Conditions)-1].Reason
+	for _, instance := range instances {
+		t.Append([]string{
+			instance.Name,
+			instance.Namespace,
+			instance.Spec.ClusterServiceClassExternalName,
+			instance.Spec.ClusterServicePlanExternalName,
+			getInstanceStatusShort(instance.Status),
+		})
 	}
-	t.Append([]string{
-		instance.Name,
-		instance.Spec.ClusterServiceClassExternalName,
-		instance.Spec.ClusterServiceClassRef.Name,
-		instance.Spec.ClusterServicePlanExternalName,
-		instance.Spec.ClusterServicePlanRef.Name,
-		latestCond,
-	})
+
+	t.Render()
 }
 
 // WriteParentInstance prints identifying information for a parent instance.
@@ -66,7 +59,7 @@ func WriteParentInstance(instance *v1beta1.ServiceInstance) {
 	t.Render()
 }
 
-// WriteAssociatedInstances prints a list of instances associated with a service plan.
+// WriteAssociatedInstances prints a list of instances associated with a plan.
 func WriteAssociatedInstances(instances *v1beta1.ServiceInstanceList) {
 	fmt.Println("\nInstances:")
 	if len(instances.Items) == 0 {
@@ -84,7 +77,7 @@ func WriteAssociatedInstances(instances *v1beta1.ServiceInstanceList) {
 		t.Append([]string{
 			instance.Name,
 			instance.Namespace,
-			getInstanceStatusFull(instance.Status),
+			getInstanceStatusShort(instance.Status),
 		})
 	}
 	t.Render()
