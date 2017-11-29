ac832ec6767f68fd16b229f78f90b47356c29ff7
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2017-11-29 10:41:36 -0600

    Standardize broker output

--- pkg/output/broker.go
+++ pkg/output/broker.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
-	"github.com/olekukonko/tablewriter"
 )
 
 func getBrokerStatusCondition(status v1beta1.ClusterServiceBrokerStatus) v1beta1.ServiceBrokerCondition {
@@ -24,42 +23,22 @@ func getBrokerStatusFull(status v1beta1.ClusterServiceBrokerStatus) string {
 	return formatStatusText(string(lastCond.Type), lastCond.Message, lastCond.LastTransitionTime)
 }
 
-// ClusterServiceBrokerHeaders sets the appropriate headers on t for displaying
-// ClusterServiceBrokers in t
-func ClusterServiceBrokerHeaders(t *tablewriter.Table) {
+// WriteBrokerList prints a list of brokers.
+func WriteBrokerList(brokers ...v1beta1.ClusterServiceBroker) {
+	t := NewListTable()
 	t.SetHeader([]string{
 		"Name",
 		"URL",
-		"Status Type",
-		"Status Value",
-		"Status Reason",
-		"Status Message",
+		"Status",
 	})
-}
-
-// AppendClusterServiceBroker appends instance to t by calling t.Append.
-// Ensure that you've called ClusterServiceBrokerHeaders on t before you call
-// this function
-func AppendClusterServiceBroker(t *tablewriter.Table, broker *v1beta1.ClusterServiceBroker) {
-	condType := ""
-	condStatus := ""
-	condReason := ""
-	condMessage := ""
-	if len(broker.Status.Conditions) >= 1 {
-		cond := &broker.Status.Conditions[len(broker.Status.Conditions)-1]
-		condType = string(cond.Type)
-		condStatus = string(cond.Status)
-		condReason = cond.Reason
-		condMessage = cond.Message
+	for _, broker := range brokers {
+		t.Append([]string{
+			broker.Name,
+			broker.Spec.URL,
+			getBrokerStatusShort(broker.Status),
+		})
 	}
-	t.Append([]string{
-		broker.Name,
-		broker.Spec.URL,
-		condType,
-		condStatus,
-		condReason,
-		condMessage,
-	})
+	t.Render()
 }
 
 // WriteParentBroker prints identifying information for a parent broker.
