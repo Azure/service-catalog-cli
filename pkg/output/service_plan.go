5b86177c34bf6ba4d39870cb57c8e1c92b219cba
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2017-11-29 10:34:03 -0600

    Standardize plan output

--- pkg/output/service_plan.go
+++ pkg/output/service_plan.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
-	"github.com/olekukonko/tablewriter"
 )
 
 func getPlanStatusShort(status v1beta1.ClusterServicePlanStatus) string {
@@ -28,30 +27,6 @@ func (a byClass) Swap(i, j int) {
 }
 func (a byClass) Less(i, j int) bool {
 	return a[i].Spec.ClusterServiceClassRef.Name < a[j].Spec.ClusterServiceClassRef.Name
-}
-
-// ClusterServicePlanHeaders sets the appropriate headers on t for displaying
-// ClusterServicePlans in t
-func ClusterServicePlanHeaders(table *tablewriter.Table) {
-	table.SetHeader([]string{
-		"Name",
-		"Description",
-		"UUID",
-		"Class Name",
-		"Class UUID",
-	})
-}
-
-// AppendClusterServicePlan appends plan to t by calling t.Append.
-// Ensure that you've called ClusterServicePlanHeaders on t before you call this function
-func AppendClusterServicePlan(table *tablewriter.Table, plan *v1beta1.ClusterServicePlan) {
-	table.Append([]string{
-		plan.Spec.ExternalName,
-		plan.Spec.Description,
-		plan.Name,
-		plan.Spec.ClusterServiceBrokerName,
-		plan.Spec.ClusterServiceClassRef.Name,
-	})
 }
 
 // WritePlanList prints a list of plans.
